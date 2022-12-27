921a97a8a8132d2712534ea2066fa4f69256c785
khushbujaviya <120018817+khushbujaviya@users.noreply.github.com>
2022-12-27 17:00:03 +0530

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,23 @@
+//get&print 1D array of n element
+
+#include<stdio.h>
+main()
+{
+	int n;
+	
+		printf("enter element n:");
+		scanf("%d",&n);
+	
+	
+		int i,a[n];
+
+		
+			for(i=0;i<n;i++)
+			
+		{
+			scanf("%d",&a[n]);
+	
+		printf("%d",a[n]);
+	}
+}
+	
---
+++ 2.2.c
@@ -0,0 +1,25 @@
+//find lenght of 1D array 
+
+#include<stdio.h>
+main()
+{
+	int n;
+	
+		printf("enter element n:");
+		scanf("%d",&n);
+	
+	
+		int i,a[n],count=0;
+
+		
+			for(i=0;i<n;i++)
+		{
+			scanf("%d\n",&a[i]);
+		}
+			count=n+1;
+			{
+				printf("%d",count);
+			}
+		
+}
+	
